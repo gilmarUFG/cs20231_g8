2efe03a8ea5a47c4e246503876bf4aa01e437777
Robert-Martins <robertcantares@gmail.com>
2023-05-05 19:44:26 -0300

    feat/api-project-init

---
+++ image-repo-api/src/test/java/com/ufg/g8/imagerepoapi/imagerepoapi/ImageRepoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufg.g8.imagerepoapi.imagerepoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageRepoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

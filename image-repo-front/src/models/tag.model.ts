2ba366e8ccb53ddb2bc90d39d6e6d6e59c4257c3
Yuan <yuancalixto@gmail.com>
2023-05-20 01:02:37 -0300

    Finalização dos Modelos

--- image-repo-front/src/models/tag.model.ts
+++ image-repo-front/src/models/tag.model.ts
@@ -1,24 +1,22 @@
 import BaseEntity from "./base.entity.model";
+import Categories from "./category.model";
 
 export default class Tag extends BaseEntity {
-
     name: string;
-
     color: string;
-
     categories: Categories[];
 
-    constructor(name: string, color: string, categories: Categories, id: string, updatedAt: Date, createdAt: Date) {
-
+    constructor(
+        id: string,
+        name: string,
+        color: string,
+        categories: Categories[],
+        updatedAt: Date,
+        createdAt: Date
+    ) {
         super(id, updatedAt, createdAt);
-
-
         this.name = name;
-
         this.color = color;
-
         this.categories = categories;
-
     }
-
 }

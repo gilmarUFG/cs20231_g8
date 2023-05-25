adf00a24ee5738b0c80eaaff1c532bf287e0a0f7
Robert-Martins <robertcantares@gmail.com>
2023-05-24 22:53:34 -0300

    feat/init-security-config-and-permit-all-requests

--- image-repo-api/src/main/java/com/ufg/g8/imagerepoapi/infrastructure/configs/SecurityConfig.java
+++ image-repo-api/src/main/java/com/ufg/g8/imagerepoapi/infrastructure/configs/SecurityConfig.java
@@ -1,4 +1,23 @@
 package com.ufg.g8.imagerepoapi.infrastructure.configs;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/public/**").permitAll()
+                .anyRequest().permitAll()
+                .and()
+                .csrf().disable();
+
+        return http.build();
+    }
+
 }

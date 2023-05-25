580653df8724dadd2617cc6f2c313cfc329c139d
Robert-Martins <robertcantares@gmail.com>
2023-05-24 22:55:49 -0300

    feat/app-user-extends-user-details

--- image-repo-api/src/main/java/com/ufg/g8/imagerepoapi/domain/models/User.java
+++ image-repo-api/src/main/java/com/ufg/g8/imagerepoapi/domain/models/User.java
@@ -6,14 +6,18 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Getter
 @Setter
 @Document(collection = "users")
-public class User extends BaseEntity {
+public class User extends BaseEntity implements UserDetails {
 
     @NotNull
-    private String login;
+    private String username;
 
     @NotNull
     private String password;
@@ -22,6 +26,30 @@ public class User extends BaseEntity {
     private String name;
 
     @DBRef
-    private File file;
+    private File profilePicture;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }

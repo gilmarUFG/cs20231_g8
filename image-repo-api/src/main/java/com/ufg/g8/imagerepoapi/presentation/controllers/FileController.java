38c6c681a7e15b337f62507492e01253f9b26ffb
Robert-Martins <robertcantares@gmail.com>
2023-05-25 15:26:58 -0300

    feat/compress-image-and-enhance-data-flows

--- image-repo-api/src/main/java/com/ufg/g8/imagerepoapi/presentation/controllers/FileController.java
+++ image-repo-api/src/main/java/com/ufg/g8/imagerepoapi/presentation/controllers/FileController.java
@@ -4,7 +4,6 @@ import com.ufg.g8.imagerepoapi.presentation.dtos.FileDto;
 import com.ufg.g8.imagerepoapi.presentation.services.IFileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 

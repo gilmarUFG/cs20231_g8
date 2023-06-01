589f073ef2b097f7276bf345df9b431ea35b73fd
Robert-Martins <robertcantares@gmail.com>
2023-06-01 16:06:32 -0300

    feat/implement-exception-handlers-and-response-details-on-error

--- image-repo-api/src/main/java/com/ufg/g8/imagerepoapi/infrastructure/exceptions/details/ExceptionDetails.java
+++ image-repo-api/src/main/java/com/ufg/g8/imagerepoapi/infrastructure/exceptions/details/ExceptionDetails.java
@@ -1,4 +1,24 @@
 package com.ufg.g8.imagerepoapi.infrastructure.exceptions.details;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class ExceptionDetails {
+
+    private String title;
+
+    private int status;
+
+    private String details;
+
+    private String developerMessage;
+
+    private String className;
+
+    private LocalDateTime timestamp;
+
 }

07025751f7cf27050f9f5f04e6092488d22121fe
Gabriel Bispo <gabriel.bispo@discente.ufg.br>
2023-06-30 22:39:13 -0300

    feat/filter-media-list-public-controller

---
+++ image-repo-api/src/main/java/com/ufg/g8/imagerepoapi/presentation/controllers/publics/MediaPublicController.java
@@ -0,0 +1,29 @@
+package com.ufg.g8.imagerepoapi.presentation.controllers.publics;
+
+import com.ufg.g8.imagerepoapi.domain.services.filters.MediaFilter;
+import com.ufg.g8.imagerepoapi.presentation.dtos.MediaDto;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/public/images")
+public class MediaPublicController {
+    @Autowired
+    private MediaPublicController mediaService;
+
+    @GetMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    private MediaDto read(@PathVariable(name = "id") ObjectId id) {
+        return this.mediaService.read(id);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    private List<MediaDto> readAll(MediaFilter filter) {
+        return this.mediaService.readAll(filter);
+    }
+}

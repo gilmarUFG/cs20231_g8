c0c79c68b6ec7fe3e2d69d18acfd6ad8af303b46
Gabriel Bispo <gabriel.bispo@discente.ufg.br>
2023-06-30 22:37:43 -0300

    feat/filter-media-list-interface

--- image-repo-api/src/main/java/com/ufg/g8/imagerepoapi/domain/repositories/MediaRepository.java
+++ image-repo-api/src/main/java/com/ufg/g8/imagerepoapi/domain/repositories/MediaRepository.java
@@ -2,9 +2,16 @@ package com.ufg.g8.imagerepoapi.domain.repositories;
 
 import com.ufg.g8.imagerepoapi.domain.models.Media;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.QueryAnnotation;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MediaRepository extends MongoRepository<Media, ObjectId> {
+
+    @Query("query with name, description")
+    List<Media> findBy(String name, String description);
 }

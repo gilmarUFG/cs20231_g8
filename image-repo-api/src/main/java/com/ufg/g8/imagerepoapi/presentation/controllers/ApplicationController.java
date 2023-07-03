c2aed166b384e0ed4baebf1a002a306fe52c3243
Robert-Martins <robertcantares@gmail.com>
2023-07-03 17:12:47 -0300

    feat/load-app-enums

---
+++ image-repo-api/src/main/java/com/ufg/g8/imagerepoapi/presentation/controllers/ApplicationController.java
@@ -0,0 +1,24 @@
+package com.ufg.g8.imagerepoapi.presentation.controllers;
+
+import com.ufg.g8.imagerepoapi.presentation.dtos.EnumDto;
+import com.ufg.g8.imagerepoapi.presentation.services.IGenericEnumService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/app")
+public class ApplicationController {
+
+    @Autowired
+    private IGenericEnumService genericEnumService;
+
+    @GetMapping("/enum/{enumType}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<EnumDto> loadEnum(@PathVariable(name = "enumType") String enumType) {
+        return this.genericEnumService.loadEnum(enumType);
+    }
+
+}

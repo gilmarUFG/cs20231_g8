7630337e723df85c47560f9b719d923f3743e169
Robert-Martins <robertcantares@gmail.com>
2023-07-25 23:38:34 -0300

    feat/define-forms-components-and-module

---
+++ image-repo-front/src/components/organisms/forms/index.ts
@@ -0,0 +1,4 @@
+export { default as LoginForm } from "./LoginForm";
+export { default as SignUpForm } from "./SignUpForm";
+export { default as MediaForm } from "./MediaForm";
+export { default as ReportForm } from "./ReportForm";

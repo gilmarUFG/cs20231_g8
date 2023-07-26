ae15b9d66f09d41dce11cf2ead3cce74df1ea51e
Robert-Martins <robertcantares@gmail.com>
2023-07-25 23:40:01 -0300

    feat/define-pages-on-routes

--- image-repo-front/src/App.tsx
+++ image-repo-front/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import "./App.css";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import {LayoutComponent, NavbarComponent} from './components';
-import {AboutPage, HomePage, TestPage, NotFoundPage} from './pages'
+import {AboutPage, HomePage, TestPage, NotFoundPage, ImagesPage, ImagePage, ProfilePage, LoginPage, SignUpPage} from './pages'
 import { ToastContainer } from 'react-toastify';
 
 export interface IApplicationProps {}
@@ -14,6 +14,10 @@ const App: React.FunctionComponent<IApplicationProps> = (props) => {
                 <NavbarComponent/>
                 <Routes>
                     <Route path="/" element={<HomePage />} />
+                    <Route path='images'>
+                        <Route index element={<ImagesPage />} />
+                        <Route path=':number' element={<ImagePage />} />
+                    </Route>
                     <Route path="about">
                         <Route index element={<AboutPage />} />
                         <Route path=":number" element={<AboutPage />} />
@@ -23,6 +27,9 @@ const App: React.FunctionComponent<IApplicationProps> = (props) => {
                         <Route index element={<AboutPage />} />
                         <Route path=":number" element={<AboutPage />} />
                     </Route>
+                    <Route path="profile" element={<ProfilePage />} />
+                    <Route path="login" element={<LoginPage />} />
+                    <Route path="sign-up" element={<SignUpPage />} />
                     <Route path="*" element={<NotFoundPage />} />
                 </Routes>
             </BrowserRouter>

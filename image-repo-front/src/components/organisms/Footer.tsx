a81287fd0636a525cdd59e6ac4f4aea5a0853229
Robert-Martins <robertcantares@gmail.com>
2023-08-03 22:53:06 -0300

    feat/implement-header-and-footer

--- image-repo-front/src/components/organisms/Footer.tsx
+++ image-repo-front/src/components/organisms/Footer.tsx
@@ -1,22 +1,47 @@
 import { styled } from "styled-components";
 import Navigation from "./Navigation";
+import { Divider } from "../atoms";
 
 type FooterProps = {}
 
 const StyledFooter = styled.footer`
-
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 24px 100px;
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
+    section {
+        display: flex;
+        gap: 96px;
+        div {
+            flex: 0.3;
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+        }
+    }
+    h6 {
+        flex: 1;
+        text-align: center;
+    }
 `;
 
 const Footer: React.FunctionComponent<FooterProps> = (props) => {
     return (
         <StyledFooter>
             <section>
-                <h4>
-                    Pixel Port
-                </h4>
-                <Navigation />
+                <div>
+                    <h3>
+                        Pixel Port
+                    </h3>
+                    <h5>
+                        Explore e Conquiste: <br/>A Plataforma Definitiva para Publicação de Imagens!
+                    </h5>
+                </div>
+                <Navigation flexColumn />
             </section>
-            <span />
+            <Divider />
             <h6>
                 2077 Pixel Port. Todos direitos reservados.
             </h6>
--- image-repo-front/src/components/organisms/Header.tsx
+++ image-repo-front/src/components/organisms/Header.tsx
@@ -1,36 +1,31 @@
 import { NavLink, NavigateFunction, useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
 import { FlatButton } from "../atoms";
+import Navigation from "./Navigation";
 
 type HeaderProps = {};
 
 const StyledHeader = styled.header`
-    display: flex;
-    align-items: center;
-    gap: 64px;
-    padding: 24px 100px;
-    background-color: #e1effc;
-    nav {
-        flex: 1;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: none;
+    > div {
+        max-width: 1280px;
+        margin: 0 auto;
+        padding: 24px 100px;
         display: flex;
-        justify-content: space-between;
-        ul {
+        align-items: center;
+        gap: 64px;
+        div {
+            flex: 1;
             display: flex;
             align-items: center;
-            gap: 24px;
-            list-style-type: none;
-            li {
-                a {
-                    font-size: 12px;
-                    color: black;
-                    text-decoration: none;
-                    transition: 0.2s;
-                    &:hover, &:focus, &.active {
-                        font-weight: 500;
-                        color: black;
-                        transition: 0.2s;
-                    }
-                }
+            justify-content: space-between;
+            > ul {
+                display: flex;
+                gap: 12px;
             }
         }
     }
@@ -42,28 +37,20 @@ const Header: React.FunctionComponent<HeaderProps> = (props) => {
 
     return (
         <StyledHeader>
-            <h3>Pixel Port</h3>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/images">Imagens</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/about">Sobre</NavLink>
-                    </li>
-                </ul>
-                <ul>
-                    <FlatButton type="button" secondary onClick={() => navigate("signup")}>
-                        <NavLink to="/signup">Sign Up</NavLink>
-                    </FlatButton>
-                    <FlatButton type="button" onClick={() => navigate("login")}>
-                        <NavLink to="/login">Login</NavLink>
-                    </FlatButton>
-                </ul>
-            </nav>
+            <div>
+                <h3>Pixel Port</h3>
+                <div>
+                    <Navigation />
+                    <ul>
+                        <FlatButton type="button" secondary onClick={() => navigate("signup")}>
+                            <NavLink to="/signup">Sign Up</NavLink>
+                        </FlatButton>
+                        <FlatButton type="button" onClick={() => navigate("login")}>
+                            <NavLink to="/login">Login</NavLink>
+                        </FlatButton>
+                    </ul>
+                </div>
+            </div>
         </StyledHeader>
     );
 };

927b72004df5fbb8c23c1efae58c7d6eba1b8f01
Robert-Martins <robertcantares@gmail.com>
2023-08-03 22:51:35 -0300

    feat/define-auth-and-splash-screen-contexts-and-providers

--- image-repo-front/src/App.tsx
+++ image-repo-front/src/App.tsx
@@ -1,39 +1,46 @@
 import React from 'react';
 import "./App.css";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Header, Footer, LayoutComponent } from './components';
+import { Header, Footer, LayoutComponent, SplashScreen } from './components';
 import { AboutPage, HomePage, TestPage, NotFoundPage, ImagesPage, ImagePage, ProfilePage, LoginPage, SignUpPage } from './pages'
 import { ToastContainer } from 'react-toastify';
+import AuthProvider from './contexts/AuthProvider';
+import SplashScreenProvider from './contexts/SplashScreenProvider';
 
 export interface IApplicationProps {}
 
 const App: React.FunctionComponent<IApplicationProps> = (props) => {
     return (
         <>
-            <BrowserRouter>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path='images'>
-                        <Route index element={<ImagesPage />} />
-                        <Route path=':number' element={<ImagePage />} />
-                    </Route>
-                    <Route path="about">
-                        <Route index element={<AboutPage />} />
-                        <Route path=":number" element={<AboutPage />} />
-                    </Route>
-                    <Route path="test" element={<TestPage />} />
-                    <Route path="layout" element={<LayoutComponent />}>
-                        <Route index element={<AboutPage />} />
-                        <Route path=":number" element={<AboutPage />} />
-                    </Route>
-                    <Route path="profile" element={<ProfilePage />} />
-                    <Route path="login" element={<LoginPage />} />
-                    <Route path="signup" element={<SignUpPage />} />
-                    <Route path="*" element={<NotFoundPage />} />
-                </Routes>
-                <Footer />
-            </BrowserRouter>
+            <SplashScreenProvider>
+                <AuthProvider>
+                        <BrowserRouter>
+                            <Header />
+                            <Routes>
+                                <Route path="/" element={<HomePage />} />
+                                <Route path='images'>
+                                    <Route index element={<ImagesPage />} />
+                                    <Route path=':number' element={<ImagePage />} />
+                                </Route>
+                                <Route path="about">
+                                    <Route index element={<AboutPage />} />
+                                    <Route path=":number" element={<AboutPage />} />
+                                </Route>
+                                <Route path="test" element={<TestPage />} />
+                                <Route path="layout" element={<LayoutComponent />}>
+                                    <Route index element={<AboutPage />} />
+                                    <Route path=":number" element={<AboutPage />} />
+                                </Route>
+                                <Route path="profile" element={<ProfilePage />} />
+                                <Route path="login" element={<LoginPage />} />
+                                <Route path="signup" element={<SignUpPage />} />
+                                <Route path="*" element={<NotFoundPage />} />
+                            </Routes>
+                            <Footer />
+                        </BrowserRouter>
+                </AuthProvider>
+                <SplashScreen />
+            </SplashScreenProvider>
             <ToastContainer/>
         </>
     );

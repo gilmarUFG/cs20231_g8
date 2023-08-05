b157e7e983d9770fdaaa1fefbbefc78328063bfd
Robert-Martins <robertcantares@gmail.com>
2023-08-04 23:53:33 -0300

    feat/home-advantages-section-init

--- image-repo-front/src/components/molecules/cards/DifferentialCard.tsx
+++ image-repo-front/src/components/molecules/cards/DifferentialCard.tsx
@@ -1,15 +1,28 @@
 import { styled } from "styled-components";
+import { DifferentialsDataType } from "../../../data/Differentials.data";
 
-type DifferentialCardProps = {};
+type DifferentialCardProps = {
+    data: DifferentialsDataType;
+};
 
 const StyledDifferentialCard = styled.div`
-
+    width: 240px;
+    justify-self: center;
+    padding: 24px;
+    border: 1px solid #747880;
+    border-radius: 4px;
 `;
 
 const DifferentialCard: React.FunctionComponent<DifferentialCardProps> = (props) => {
     return (
         <StyledDifferentialCard>
-
+            {props.data.icon({ color: props.data.color })}
+            <h5>
+                {props.data.title}
+            </h5>
+            <h6>
+                {props.data.subtitle}
+            </h6>
         </StyledDifferentialCard>
     )
 }
--- image-repo-front/src/components/molecules/containers/AppGrid.tsx
+++ image-repo-front/src/components/molecules/containers/AppGrid.tsx
@@ -1,13 +1,22 @@
 import { styled } from "styled-components";
 
-type AppGridProps = {}
-
-const AppGrid: React.FunctionComponent<AppGridProps> = (props) => {
-    return (<></>);
+type AppGridProps = {
+    columns?: number;
 }
 
-const StyledAppGrid = styled(AppGrid)`
-
+const AppGrid = styled.div<AppGridProps>`
+    display: grid;
+    grid-template-columns: repeat(${props => props.columns ? +props.columns : '3'}, 1fr);
+    gap: 24px;
+    & .big-item {
+        grid-column: 1/-1;
+    }
+    & .md-item {
+        grid-column: span 2;
+    }
+    & .small-item {
+        grid-column: span 1;
+    }
 `;
 
-export default StyledAppGrid;
+export default AppGrid;

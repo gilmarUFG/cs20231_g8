70716b0e0f48826717701f1629de6c6a197014ed
Robert-Martins <robertcantares@gmail.com>
2023-08-11 19:49:29 -0300

    feat/sign-up-page-finish-without-user-image

--- image-repo-front/src/pages/SignUpPage.tsx
+++ image-repo-front/src/pages/SignUpPage.tsx
@@ -1,7 +1,37 @@
-type SignUpPageProps = {}
+import { styled } from "styled-components";
+import { AppContainer, SignUpForm } from "../components";
+
+type SignUpPageProps = {};
+
+const StyledSignUpPage = styled.section`
+    width: 100%;
+    padding: 144px 72px 72px 72px;
+    background-image: url("./../assets/svgs/meteor-bg.svg");
+    > div {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        gap: 72px;
+        h1 {
+            flex: 0.5;
+        }
+        > div {
+            flex: 0.3;
+        }
+    }
+`;
 
 const SignUpPage: React.FunctionComponent<SignUpPageProps> = (props) => {
-    return <p>é pra ser a página de cadastro de usuário</p>
+    return (
+        <StyledSignUpPage>
+            <AppContainer>
+                <h1>
+                    Junte-se à Nossa Comunidade Visual: O Seu Espaço para Compartilhar e Brilhar!
+                </h1>
+                <SignUpForm />
+            </AppContainer>
+        </StyledSignUpPage>
+    );
 }
 
 export default SignUpPage;

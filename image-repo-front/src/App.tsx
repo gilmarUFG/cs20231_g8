13be7146da6351a611103d9043379614117ce557
Robert-Martins <robertcantares@gmail.com>
2023-08-11 19:47:33 -0300

    refactor/enhance-component-disposal-on-app-tsx

--- image-repo-front/src/App.tsx
+++ image-repo-front/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import "./App.css";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Header, Footer, LayoutComponent, SplashScreen } from './components';
-import { AboutPage, HomePage, TestPage, NotFoundPage, ImagesPage, ImagePage, ProfilePage, LoginPage, SignUpPage } from './pages'
+import { Header, Footer, SplashScreen } from './components';
+import { AboutPage, HomePage, NotFoundPage, ImagesPage, ImagePage, ProfilePage, LoginPage, SignUpPage } from './pages'
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import AuthProvider from './contexts/AuthProvider';
 import SplashScreenProvider from './contexts/SplashScreenProvider';
 
@@ -22,15 +23,7 @@ const App: React.FunctionComponent<IApplicationProps> = (props) => {
                                     <Route index element={<ImagesPage />} />
                                     <Route path=':number' element={<ImagePage />} />
                                 </Route>
-                                <Route path="about">
-                                    <Route index element={<AboutPage />} />
-                                    <Route path=":number" element={<AboutPage />} />
-                                </Route>
-                                <Route path="test" element={<TestPage />} />
-                                <Route path="layout" element={<LayoutComponent />}>
-                                    <Route index element={<AboutPage />} />
-                                    <Route path=":number" element={<AboutPage />} />
-                                </Route>
+                                <Route path="about" element={<AboutPage />} />
                                 <Route path="profile" element={<ProfilePage />} />
                                 <Route path="login" element={<LoginPage />} />
                                 <Route path="signup" element={<SignUpPage />} />

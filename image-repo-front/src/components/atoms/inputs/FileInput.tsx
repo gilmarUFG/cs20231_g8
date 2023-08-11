8e1012ad45955607e75035d5b93a4d6104d52921
Robert-Martins <robertcantares@gmail.com>
2023-08-11 19:48:03 -0300

    feat/implement-input-and-define-file-input

---
+++ image-repo-front/src/components/atoms/inputs/FileInput.tsx
@@ -0,0 +1,10 @@
+import { styled } from "styled-components";
+import InputProps from "../../../types/input-props.type";
+
+const FileInput: React.FunctionComponent<InputProps> = (props) => {
+    return (
+        <></>
+    );
+}
+
+export default FileInput;
